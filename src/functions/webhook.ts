c754c0d21f001123c7efdf2d9ea36670865dfcea
17meyer <7meyer@informatik.uni-hamburg.de>
2025-10-08 00:26:01 +0200

    add basic auth

--- src/functions/webhook.ts
+++ src/functions/webhook.ts
@@ -1,10 +1,27 @@
 import { Handler } from "@netlify/functions";
 
-const handler: Handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: "ok",
-  };
+const handler: Handler = async (event) => {
+
+  const authHeader = event.headers["authorization"];
+
+  //    username:password -> base64 encode
+  const expectedUser = process.env.WEBHOOK_USER!;
+  const expectedPass = process.env.WEBHOOK_PASS!;
+  const expectedAuth =
+    "Basic " + Buffer.from(`${expectedUser}:${expectedPass}`).toString("base64");
+
+  if (authHeader !== expectedAuth) {
+    return {
+      statusCode: 401,
+      headers: { "WWW-Authenticate": 'Basic realm="Protected"' },
+      body: "Unauthorized",
+    };
+  }
+
+  const payload = event.body ? JSON.parse(event.body) : {};
+  console.log("Verified webhook:", payload);
+
+  return { statusCode: 200, body: "ok" };
 };
 
 export { handler };
